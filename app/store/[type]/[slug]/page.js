c28419839d7e79b0f661f93dee100a9db270d0ca
RightHacker <viratpoddar2012@gmail.com>
2026-04-15 10:01:28 +0530

    fetch fixed

--- app/store/[type]/[slug]/page.js
+++ app/store/[type]/[slug]/page.js
@@ -5,7 +5,6 @@ export async function generateMetadata({ params }) {
 
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/items/${slug}`,
-    { cache: "no-store" },
   );
 
   const data = await res.json();
